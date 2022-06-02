56095441eecd933d0d038c056acb5008c470f339
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-06-03 01:09:28 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
  * main - main block
  * Return: 0
  */
-int main(void)
-{
-puts("Programming is like building a mutilingual puzzle");
+int main(void){
+puts("\"Programming is like building a mutilingual puzzle");
 return(0);
 }
