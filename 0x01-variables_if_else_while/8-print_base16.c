5112a996f78c0cd2d4c7a89a8aa2cdbb79755dd1
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-06-12 03:14:59 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Print all numbers of base64 in lowercase.
- * You can only use `putchar`, and only 3 times.
- * Return: 0
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c;
-	int i;
+	int n;
+	char ch;
 
-	for (i = 0; i < 10; i++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(i + '0');
+		putchar(n);
 	}
-	for (c = 'a'; c < 'g'; c++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
-		putchar(c);
+		putchar(ch);
 	}
 	putchar('\n');
-
 	return (0);
 }
