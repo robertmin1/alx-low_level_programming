019a45d9246a254adcdd4c5fecc419ac545594f0
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-06-12 03:17:59 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Print all possible combinations of two digits.
- * Return: 0
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j, k;
+	int i, e;
 
-	i = 0;
+	i = 48;
+	e = 48;
 
-	while (i < 100)
+	while (e < 58)
 	{
-		j = i % 10; /* singles digit */
-		k = i / 10; /* doubles digit */
-
-		if (k < j)
+		i = 48;
+		while (i < 58)
 		{
-			putchar(k + '0');
-			putchar(j + '0');
-
-			if (i < 89)
+			if (e != i && e < i)
 			{
-				putchar(44);
-				putchar(32);
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
 			}
+			i++;
 		}
-
-		i++;
+		e++;
 	}
 	putchar('\n');
-
 	return (0);
 }
