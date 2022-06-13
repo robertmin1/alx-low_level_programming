5d6b8bf40849a666fad6430f8592ab3fe9cc931e
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-06-14 01:28:58 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 
 /*
-*reset_to_98 - A function that takes the pointer of an integer and changes it to 98
-*
+*reset_to_98-A function that takes the pointer of an int and changes it to 98
+*@n: int type pointer
 *Return: Always 0.
 */
 
