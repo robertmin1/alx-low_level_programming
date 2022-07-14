d3ae2dc45fedb565b178a21a60e02c09f64fe874
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-07-14 23:58:21 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - Returns the number of elements in a linked `list_t` list
  * @h: head of linked list
- * Return: number of elements in list
+ * Return: no. of elements in list
  */
 size_t list_len(const list_t *h)
 {
